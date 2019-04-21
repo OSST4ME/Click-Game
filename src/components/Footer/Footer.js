0eef60abacdc7981a9e17e5be3aeab1697bee1b7
OSST4ME <kimmeyson@gmail.com>
2019-04-20 19:13:50 -0700

    updates

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+//sets up the reusable Footer component
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+	<header className = "footer">
+		<p>Fun With Fish</p>
+	</header>
+);
+
+export default Footer;
